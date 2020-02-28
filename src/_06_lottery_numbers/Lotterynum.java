2d76b7850f6806fee93d69a761bad3b783794d19
SMHS-519-02 <SMHS-519-02@SMHS-519-02.smhsrop.lab>
2020-02-27 16:05:31 -0800

    I work on the lottery ticket and kinda brunt forced it.

--- src/_06_lottery_numbers/Lotterynum.java
+++ src/_06_lottery_numbers/Lotterynum.java
@@ -1,10 +1,31 @@
 package _06_lottery_numbers;
 
-public class Lotterynum {
+import java.awt.print.Printable;
+import java.util.Random;
 
+public class Lotterynum {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int num = 0;
+		int num2 = 0;
+		int num3 = 0;
+		int num4 = 0;
+		int num5 = 0;
+		int numtrue= 0;
+		Random ran = new Random();
+        num = ran.nextInt(6);
+        num = num*10000;
+        num2 = ran.nextInt(6);
+        num2 = num2*1000;
+        num3 = ran.nextInt(6);
+        num3 = num3*100;
+        num4 = ran.nextInt(6);  
+        num4 = num4*10;
+        num5 = ran.nextInt(6);
+        num5 = num5*1;
+        numtrue = num + num2 + num3 + num4 + num5;
+		System.out.println(numtrue);
 	}
+
 
 }
